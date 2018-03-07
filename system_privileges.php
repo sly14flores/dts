e05ea4d01125b018ee53bacef9cb6d0e57cee32e
Sly Flores <sly@christian.com.ph>
2018-03-07 16:33:28 +0800

    privileges

---
+++ system_privileges.php
@@ -0,0 +1,23 @@
+<?php
+
+define('system_privileges', array(
+	array(
+		"id"=>1,
+		"description"=>"Dashboard",
+		"privileges"=>array( # id=1 must be always page access
+			array("id"=>1,"description"=>"Show Dashboard","value"=>false),
+		),
+	),
+	array(
+		"id"=>2,
+		"description"=>"Receive Document",
+		"privileges"=>array(
+			array("id"=>1,"description"=>"Show Receive Document","value"=>false),
+			array("id"=>2,"description"=>"Add Document","value"=>false),
+			array("id"=>3,"description"=>"Edit Document","value"=>false),
+			array("id"=>4,"description"=>"Delete Document","value"=>false),
+		),
+	),
+));
+
+?>
