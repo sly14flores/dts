eeb41bcbdac87506f1d218a412dfea43532a313b
Sly Flores <sly@christian.com.ph>
2018-03-09 15:00:22 +0800

    added group encryption

--- handlers/groups-save.php
+++ handlers/groups-save.php
@@ -3,13 +3,16 @@
 $_POST = json_decode(file_get_contents('php://input'), true);
 
 require_once '../db.php';
+require_once '../classes.php';
 
 $con = new pdo_db("groups");
 
 $privileges = [];
 if (isset($_POST['privileges'])) {
 	
-	$privileges = json_encode($_POST['privileges']);
+	$arrayHex = new ArrayHex();
+		
+	$privileges = $arrayHex->toHex(json_encode($_POST['privileges']));
 	$_POST['group']['privileges'] = $privileges;
 	
 };
