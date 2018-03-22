69f6d0ac0fafc0f67171356506b65b6efb3f7545
Sly Flores <sly@christian.com.ph>
2018-03-22 17:02:48 +0800

    ...

--- handlers/doc-transaction.php
+++ handlers/doc-transaction.php
@@ -1,5 +1,23 @@
 <?php
 
+$_POST = json_decode(file_get_contents('php://input'), true);
 
+require_once '../db.php';
+
+session_start();
+
+$con = new pdo_db("tracks");
+
+$track = array(
+	"document_id"=>$_POST['id'],
+	"system_document_status"=>"transaction",
+	"track_office"=>$_POST['track_office'],
+	"track_office_next"=>$_POST['next']['track_office_next']['id'],
+	"document_activity"=>"Received",
+	"document_activity_user"=>$_SESSION['id'],
+	"document_activity_date"=>"CURRENT_TIMESTAMP"
+);
+
+// $con->insertData($track);
 
 ?>
