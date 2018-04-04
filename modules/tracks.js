52ff2f122926b23013d0f5fa848ef454bdb272da
Sly Flores <sly@christian.com.ph>
2018-04-04 11:46:32 +0800

    added field in tracks

--- modules/tracks.js
+++ modules/tracks.js
@@ -46,7 +46,11 @@ angular.module('app-module', ['form-validator','bootstrap-modal','ui.bootstrap']
 		};	
 
 		self.track = function(scope) {
-
+			
+			$('#track').html('');
+			
+			if (scope.document.id === undefined) return;
+			
 			var loading = '<div class="col-lg-12">Fetching document tracks please wait...</div>';
 			
 			$('#track').html(loading);
@@ -57,7 +61,8 @@ angular.module('app-module', ['form-validator','bootstrap-modal','ui.bootstrap']
 			  data: {id: scope.document.id}
 			}).then(function mySuccess(response) {
 				
-				scope.tracks = response.data;
+				delete scope.document.id;
+				scope.tracks = response.data;				
 				
 				$('#track').load('html/tracks.html',function() {
 					$timeout(function() { $compile($('#track')[0])(scope); }, 100);
