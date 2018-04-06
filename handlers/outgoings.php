8eaaa7c610f0b03ccc6317fb23a5085123f4551a
Sly Flores <sly@christian.com.ph>
2018-04-06 10:22:30 +0800

    ...

--- handlers/outgoings.php
+++ handlers/outgoings.php
@@ -10,14 +10,27 @@ $con = new pdo_db("tracks");
 
 $filter = "WHERE documents.origin = ".$_SESSION['office'];
 
-$sql = "SELECT documents.id, documents.barcode, documents.doc_name, tracks.document_status, tracks.document_status_user, tracks.document_tracks_status, tracks.track_office, tracks.track_date, tracks.remarks, (SELECT document_types.document_type FROM document_types WHERE document_types.id = documents.doc_type) doc_type, (SELECT offices.office FROM offices WHERE offices.id = documents.origin) origin, (SELECT transactions.transaction FROM transactions WHERE transactions.id = documents.document_transaction_type) document_transaction_type FROM documents LEFT JOIN tracks ON documents.id = tracks.document_id {$filter} ORDER BY tracks.track_date DESC LIMIT 1";
+$sql = "SELECT documents.id, documents.barcode, documents.doc_name, tracks.document_status, tracks.document_status_user, tracks.document_tracks_status, tracks.track_office, tracks.track_date, tracks.track_option, tracks.route_office, tracks.route_user, tracks.remarks, (SELECT document_types.document_type FROM document_types WHERE document_types.id = documents.doc_type) doc_type, (SELECT offices.office FROM offices WHERE offices.id = documents.origin) origin, (SELECT transactions.transaction FROM transactions WHERE transactions.id = documents.document_transaction_type) document_transaction_type FROM documents LEFT JOIN tracks ON documents.id = tracks.document_id {$filter} ORDER BY tracks.track_date DESC LIMIT 1";
 $outgoings = $con->getData($sql);
 
 foreach ($outgoings as $i => $outgoing) {
 
-	$tracks_status = "";
+	if ($outgoing['document_tracks_status'] == "for_pick_up") {
+		$track_office = $con->getData("SELECT id, office FROM offices WHERE id = ".$outgoing['track_office']);		
+		$outgoings[$i]['document_status'] = "For pick up from ".$track_office[0]['office'];
+	};	
 
-	$outgoings[$i]['tracks_status'] = $tracks_status;
+	if ($outgoing['document_tracks_status'] == "incoming") {
+		$track_office = $con->getData("SELECT id, office FROM offices WHERE id = ".$outgoing['track_office']);		
+		$route_user = $con->getData("SELECT id, CONCAT(fname, ' ', lname) fullname FROM users WHERE id = ".$outgoing['route_user']);		
+		$outgoings[$i]['document_status'] = "Picked up from ".$track_office[0]['office']." by ".$route_user[0]['fullname'];
+	};
+
+	if ($outgoing['track_option'] != NULL) {
+		$track_option = $con->getData("SELECT id, choice FROM options WHERE id = ".$outgoing['track_option']);
+		$outgoings[$i]['document_status'] = $track_option[0]['choice'];
+	};	
+	
 	$outgoings[$i]['track_date'] = date("F j, Y",strtotime($outgoing['track_date']));
 
 };
