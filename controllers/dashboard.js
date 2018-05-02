7aa701831ea7f94123d7f6cf03e75b8a3b346bf9
Sly Flores <sly@christian.com.ph>
2018-05-02 11:45:49 +0800

    setup dashboard

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,12 +1,12 @@
-var app = angular.module('dashboard',['account-module','notifications-module']);
+var app = angular.module('dashboard',['account-module','dashboard-module','notifications-module']);
 
-app.controller('dashboardCtrl',function($scope) {
-	
+app.controller('dashboardCtrl',function($scope,dashboard) {
+
 	$scope.module = {
 		id: 'dashboard',
 		privileges: {
 
 		}
 	};
-	
+
 });
---
+++ modules/dashboard.js
@@ -0,0 +1,10 @@
+angular.module('dashboard-module', ['ui.bootstrap','bootstrap-modal','block-ui','module-access']).factory('dashboard', function($http,$timeout,$compile,$window,bootstrapModal,bui,access) {
+	
+	function dashboard() {
+		
+		
+	};
+	
+	return new dashboard();
+	
+});
