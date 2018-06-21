c0c6ecaf13f367a293275f24f20d002a9bac8acb
Sly Flores <sly@christian.com.ph>
2018-06-21 12:58:04 +0800

    ...

--- handlers/doc-transaction.php
+++ handlers/doc-transaction.php
@@ -88,7 +88,7 @@ switch ($_POST['action']) {
 				"doc_id"=>$_POST['id'],
 				"user_id"=>$liaison['id'],
 				"notification_type"=>"incoming",
-				"message"=>$_POST['doc_type']." with subject: <strong>".$_POST['document']['doc_name']."</strong> is ready for pick up at $track_office_name<br>Date: ".date("F j, Y",strtotime($track_date))."<br>Time: ".date("h:i A",strtotime($track_date))
+				"message"=>$_POST['document']['doc_type']." with subject: <strong>".$_POST['document']['doc_name']."</strong> is ready for pick up at $track_office_name<br>Date: ".date("F j, Y",strtotime($track_date))."<br>Time: ".date("h:i A",strtotime($track_date))
 			);
 
 		};
@@ -101,7 +101,7 @@ switch ($_POST['action']) {
 				"doc_id"=>$_POST['id'],
 				"user_id"=>$ao['id'],
 				"notification_type"=>"outgoing",
-				"message"=>$_POST['doc_type']." with subject: <strong>".$_POST['document']['doc_name']."</strong> was marked ready for pick up by ".$staff[0]['fullname']." at $track_office_name<br>Date: ".date("F j, Y",strtotime($track_date))."<br>Time: ".date("h:i A",strtotime($track_date))
+				"message"=>$_POST['document']['doc_type']." with subject: <strong>".$_POST['document']['doc_name']."</strong> was marked ready for pick up by ".$staff[0]['fullname']." at $track_office_name<br>Date: ".date("F j, Y",strtotime($track_date))."<br>Time: ".date("h:i A",strtotime($track_date))
 			);
 
 		};	
